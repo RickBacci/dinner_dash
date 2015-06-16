fe5f7ae2b80381c0dd1ec857917542cee5d4204a
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-06-16 11:52:31 -0600

    Add multiple items to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,10 @@
 class CartsController < ApplicationController
   def create
+    cart = session[:cart] || {}
+    cart[params[:item_id]] ||= 0
+    cart[params[:item_id]] += 1
+    session[:cart] = cart
+    
     item = Item.find(params[:item_id].to_i)
     redirect_to items_path, notice: "Added #{item.title} to cart"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :carts, only: [:create]
+  resources :carts, only: [:create, :show]
 end
