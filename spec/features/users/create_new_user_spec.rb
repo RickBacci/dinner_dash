549f8f6907d524018243a7f7bc3361ccb63dae17
Rick Bacci <rickbacci@gmail.com>
2015-06-15 18:16:47 -0600

    add create_new_user feature test

---
+++ spec/features/users/create_new_user_spec.rb
@@ -0,0 +1,22 @@
+# When I visit the new user page ‘/user/new'
+# I fill out a form with my Username, and Password
+# And click on ‘Create New User’
+# I’m re-directed to my show page
+# And I expect to see my username
+
+require 'rails_helper'
+
+RSpec.describe User, type: :feature do
+  it 'registers for the site' do
+    visit new_user_page
+
+    fill_in "Username", with: "user"
+    fill_in "Password", with: "pass"
+
+    click_button "Create User"
+
+    expect(page).to have_content("Welcome, user")
+  end
+
+   
+end
