2bbc7bcdd31ddd9cb77a27bcade7640ca3054f11
Rick Bacci <rickbacci@gmail.com>
2015-06-15 20:39:24 -0600

    remove user story from spec

--- spec/features/users/create_new_user_spec.rb
+++ spec/features/users/create_new_user_spec.rb
@@ -1,9 +1,3 @@
-# When I visit the new user page ‘/user/new'
-# I fill out a form with my Username, and Password
-# And click on ‘Create New User’
-# I’m re-directed to my show page
-# And I expect to see my username
-
 require 'rails_helper'
 
 RSpec.describe User, type: :feature do
