2d993977413387f01a8e85a226890d54a8909629
Rick Bacci <rickbacci@gmail.com>
2015-06-16 11:17:10 -0600

    hound fix

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :feature do
-
   it 'should let a registered user login to the site' do
 
     user = User.create(username: "user",
@@ -17,9 +16,3 @@ RSpec.describe User, type: :feature do
     expect(page).to have_content("Welcome, user")
   end
 end
-
-# When I visit the login page ‘/login’
-# And I fill in my username and password
-# And I click on ‘Login’
-# I’m re-directed to my show page ‘/user/:id’
-# I expect to see my username
