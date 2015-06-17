ba9ebab51979a6f5e4360137a1e2cb60d8d53532
Rick Bacci <rickbacci@gmail.com>
2015-06-17 15:00:44 -0600

    fix error in my test logic

--- spec/features/admin/admin_login_spec.rb
+++ spec/features/admin/admin_login_spec.rb
@@ -12,15 +12,15 @@ RSpec.describe User, type: :feature do
     visit root_path
 
     expect(admin.email_address).to eq('admin@site.com')
-    expect(page).to_not have_content('Admin Dashboard')
+    expect(page).to have_content('Admin Dashboard')
   end
 
   it 'should not allow a regular user to see admin functionality' do
-    user = User.new(name: 'admin',
+    user = User.new(name: 'user',
                      email_address: 'admin@site.com',
-                     username: 'admin',
-                     password: 'admin',
-                     role: 1)
+                     username: 'user',
+                     password: 'user',
+                     role: 0)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit root_path
