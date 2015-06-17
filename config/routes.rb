89aa63eb46282162665f4c41b4493188d1c5e4d4
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-17 14:31:34 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :items, only: [:show, :index]
 
-  resources :categories, only: [:index] do
+  resources :categories, only: [:index, :show] do
     resources :items, only: [:index, :show]
   end
 
