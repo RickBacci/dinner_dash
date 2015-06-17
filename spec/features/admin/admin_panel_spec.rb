c482ee7d634edeba070567795e127b417fe2b566
Rick Bacci <rickbacci@gmail.com>
2015-06-17 16:10:01 -0600

    add admin panel spec
    * add Admin::BaseController
    * add Admin::DashboardController
    * add 'Admin Dashboard' link to root_path
    * add Admin dashboard index action

--- spec/features/admin/admin_panel_spec.rb
+++ spec/features/admin/admin_panel_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Admin, type: :feature do
+describe "Admin", type: :feature do
+  it 'can visit the dashboard' do
+    admin = User.create(name: 'admin',
+                        email_address: 'admin@site.com',
+                        username: 'admin',
+                        password: 'admin',
+                        role: 1)
 
+    allow_any_instance_of(ApplicationController)
+      .to receive(:current_user)
+      .and_return(admin)
+
+    visit root_path
+
+    click_button "Admin Dashboard"
+
+    expect(page).to have_content("Admin Dashboard")
+  end
 end
