6f924ed4c4daf2ae6ff1a7773fd13f7d546bfe75
Rick Bacci <rickbacci@gmail.com>
2015-06-16 18:12:26 -0600

    update login spec - fix path closes#
    * add categories#index as root_path
    * add change redirect to root_path
    * add CategoriesController
    * add index action with @user
    * remove "Welcome, user" from user show page
    * add set session[:user_id] when new user created
    * add helper method to check for un-authenticated
    visitor
    * add categories#index(root) view

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApplicationController
+  def index
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
 
+  root 'categories#index'
+  #
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
