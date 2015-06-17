1308cb813af0bf1cd49c9eb48a7ec85a0bd7c282
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-17 11:34:58 -0600

    fix failing tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-<<<<<<< HEAD
     @user = User.find(session[:user_id]) if session[:user_id]
-=======
     @categories = Category.all
->>>>>>> 60bb67578be37cd6c87ceeb1030971c931962548
   end
 end
