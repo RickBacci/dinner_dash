024dc97a617be081d118e21e51fea09c1a420766
Rick Bacci <rickbacci@gmail.com>
2015-06-17 00:13:28 -0600

    add admin login spec

--- spec/features/admin/admin_login_spec.rb
+++ spec/features/admin/admin_login_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe User, type: :feature do
                      role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit root_path
 
+    expect(admin.email_address).to eq('admin@site.com')
     expect(page).to have_content('Welcome Admin')
   end
 end
