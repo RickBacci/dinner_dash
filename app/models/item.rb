466b253194a479595481e5e53f239522ba1cce68
Rick Bacci <rickbacci@gmail.com>
2015-06-17 23:16:43 -0600

    add migration for paperclip
    * add has_attached_file relationship to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
   has_many :category_items
   has_many :categories, through: :category_items
+
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617200131) do
+ActiveRecord::Schema.define(version: 20150618051045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,23 +22,28 @@ ActiveRecord::Schema.define(version: 20150617200131) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "order_items", force: :cascade do |t|
+  create_table "category_items", force: :cascade do |t|
     t.integer  "item_id"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "order_items", ["category_id"], name: "index_order_items_on_category_id", using: :btree
-  add_index "order_items", ["item_id"], name: "index_order_items_on_item_id", using: :btree
+  add_index "category_items", ["category_id"], name: "index_category_items_on_category_id", using: :btree
+  add_index "category_items", ["item_id"], name: "index_category_items_on_item_id", using: :btree
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.decimal  "price"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +55,6 @@ ActiveRecord::Schema.define(version: 20150617200131) do
     t.integer  "role",            default: 0
   end
 
-  add_foreign_key "order_items", "categories"
-  add_foreign_key "order_items", "items"
+  add_foreign_key "category_items", "categories"
+  add_foreign_key "category_items", "items"
 end
