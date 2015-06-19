2a662f245cce9ae31d7b3141275351e2fbabb3c3
Rick Bacci <rickbacci@gmail.com>
2015-06-19 15:47:04 -0600

    fix syntax errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,8 +17,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-
- config.before(:suite) do
+  config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
 
@@ -37,7 +36,6 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -63,8 +61,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
