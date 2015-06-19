ecfe1f0c496e7f882db58df9311c015bb96020b3
Rick Bacci <RickBacci@users.noreply.github.com>
2015-06-19 01:50:29 -0600

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,16 @@ class Item < ActiveRecord::Base
   has_many :orders, through: :order_items
 
   has_attached_file :picture,
-    styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' },
-    default_url: "default-medium.png",
-    storage: :s3,
-    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
+                    styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' },
+                    default_url: "default-medium.png",
+                    storage: :s3,
+                    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
   def s3_credentials
-    { access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    { 
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      bucket: ENV['S3_BUCKET'],
+      bucket: ENV['S3_BUCKET']
     }
   end
 
