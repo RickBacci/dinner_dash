409b56d8b722db6289afc8bf6099dc4bd4f6539e
Rick Bacci <rickbacci@gmail.com>
2015-06-20 10:16:47 -0600

    remove s3_credentials method from environments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,5 @@
 Rails.application.configure do
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    path: "/image/:id/:filename",
-    url: "/system/:attachment/:id/:style/:filename",
-    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
-  }
-
   def s3_credentials
     {
       s3_host_name: ENV["S3_HOST_NAME"],
@@ -15,6 +8,18 @@ Rails.application.configure do
       bucket: ENV["S3_BUCKET"],
     }
   end
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    path: "/image/:id/:filename",
+    url: "/system/:attachment/:id/:style/:filename",
+    s3_credentials: {
+      s3_host_name: ENV["S3_HOST_NAME"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      bucket: ENV["S3_BUCKET"],
+    }
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
