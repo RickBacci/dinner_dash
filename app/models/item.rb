0dee291a2b965236936f6b1f49645bdfa888cce3
Rick Bacci <rickbacci@gmail.com>
2015-06-25 13:18:24 -0600

    remove admin deletes item spec and action
    * remove old code

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,16 +15,4 @@ class Item < ActiveRecord::Base
   validates :description, presence: true
   validates :price, presence: true, numericality: { only: :decimal, greater_than: 0 }
   validates :categories, presence: true
-
-  # def category_list=(categories_string)
-  #   category_names = categories_string.split(",").collect{|s| s.strip.downcase}.uniq
-  #   new_or_found_categories = category_names.collect { |name| Category.find_or_create_by(name: name) }
-  #   self.categories = new_or_found_categories
-  # end
-  #
-  # def category_list
-  #   self.categories.collect do |category|
-  #     category.name
-  #   end.join(", ")
-  # end
 end
